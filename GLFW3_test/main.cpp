35f7d84ee158f58f25cde80e2c128b56b9dd4b70
omikun <omikun@gmail.com>
2014-03-18 22:18:22 -0400

    Initial Commit

---
+++ GLFW3_test/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GLFW3_test
+//
+//  Created by Si Li on 3/18/14.
+//  Copyright (c) 2014 Si Li. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+

d42ec52fd7170b7c7e37f067a5d9bce308dc1326
omikun <omikun@gmail.com>
2014-09-07 22:33:24 -0400

    first successful port of simpleOpengLTest (shader version of font stash) with glfw3, works with retina! sort of… coordinates for text is messed up when switching between dpi modes (retina to non retina)

--- GLFW3_test/main.cpp
+++ GLFW3_test/main.cpp
@@ -7,17 +7,216 @@
 //
 
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <numeric>
+#include <vector>
+#include <chrono>
+#include <cassert>
+#include <iomanip>
+
 #define GLFW_INCLUDE_GLCOREARB
 //#include <OpenGL/glu.h>
 #include "includes.h" 
 #include "ogl.h"
 #include <GLFW/glfw3.h>
+
+#include "../fontstash/fontstash.h"
+#include "../fontstash/opengl_fontstashcallbacks.h"
+#include "../rendertest/LoadShader.h"
+#include "../rendertest/GLPrimInternalData.h"
+
+#define CUSTOM_VSYNC 2
+#define VSYNC 1
+
+using namespace std;
+
+static PrimInternalData sData;
+
 void errorcb(int error, const char* desc)
 {
 	printf("GLFW error %d: %s\n", error, desc);
 }
+
+void loadShader();
+unsigned int indexData[6] = {0,1,2,0,2,3};
+
+void loadBufferData(){
+    
+    Vertex vertexData[4] = {
+        { vec4(-0.5, -0.5, 0.0, 1.0 ), vec4( 1.0, 1.0, 1.0, 1.0 ) ,vec2(0.0078125,0.015625)},
+        { vec4(-0.5,  0.5, 0.0, 1.0 ), vec4( 1.0, 1.0, 1.0, 1.0 ) ,vec2(0.101562,0.015625)},
+        { vec4( 0.5,  0.5, 0.0, 1.0 ), vec4( 1.0, 1.0, 1.0, 1.0 ) ,vec2(0.101562,0.105469)},
+        { vec4( 0.5, -0.5, 0.0, 1.0 ), vec4( 1.0, 1.0, 1.0, 1.0 ) ,vec2(0.0078125,0.105469)}
+    };
+    
+    
+    
+	glGenVertexArrays(1, &sData.m_vertexArrayObject);
+    glBindVertexArray(sData.m_vertexArrayObject);
+    
+    glGenBuffers(1, &sData.m_vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, sData.m_vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, 4 * sizeof(Vertex), vertexData, GL_STATIC_DRAW);
+    GLuint err = glGetError();
+    assert(err==GL_NO_ERROR);
+    
+    
+    
+    glGenBuffers(1, &sData.m_indexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, sData.m_indexBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER,6*sizeof(int), indexData,GL_STATIC_DRAW);
+    
+    glEnableVertexAttribArray(sData.m_positionAttribute);
+    glEnableVertexAttribArray(sData.m_colourAttribute);
+	err = glGetError();
+    assert(err==GL_NO_ERROR);
+    
+	glEnableVertexAttribArray(sData.m_textureAttribute);
+    
+    glVertexAttribPointer(sData.m_positionAttribute, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const GLvoid *)0);
+    glVertexAttribPointer(sData.m_colourAttribute  , 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const GLvoid *)sizeof(vec4));
+    glVertexAttribPointer(sData.m_textureAttribute , 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const GLvoid *)(sizeof(vec4)+sizeof(vec4)));
+	err = glGetError();
+    assert(err==GL_NO_ERROR);
+    
+}
+
+void initTestTexture()
+{
+    //	glEnable(GL_TEXTURE_2D);
+	glGenTextures(1,(GLuint*)&sData.m_texturehandle);
+	
+    GLint err = glGetError();
+    assert(err==GL_NO_ERROR);
+    
+    glBindTexture(GL_TEXTURE_2D,sData.m_texturehandle);
+    
+    err = glGetError();
+    assert(err==GL_NO_ERROR);
+    
+	int width=256;
+	int height=256;
+	unsigned char* image = (unsigned char*)malloc(width*height);
+	memset(image,0,width*height);
+	for (int i=0;i<width;i++)
+	{
+		for (int j=0;j<height;j++)
+		{
+			if (i==j)
+				image[i+width*j]=0;
+			else
+				image[i+width*j]=255;
+		}
+	}
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, width,height,0,GL_RED,GL_UNSIGNED_BYTE,image);
+	
+    err = glGetError();
+    assert(err==GL_NO_ERROR);
+    
+    glGenerateMipmap(GL_TEXTURE_2D);
+	
+    err = glGetError();
+    assert(err==GL_NO_ERROR);
+    
+    free(image);
+    
+}
+
+
+static const char* vertexShader= \
+"#version 150   \n"
+"\n"
+"uniform vec2 p;\n"
+"\n"
+"in vec4 position;\n"
+"in vec4 colour;\n"
+"out vec4 colourV;\n"
+"\n"
+"in vec2 texuv;\n"
+"out vec2 texuvV;\n"
+"\n"
+"\n"
+"void main (void)\n"
+"{\n"
+"    colourV = colour;\n"
+"	gl_Position = vec4(p.x+position.x, p.y+position.y,0.f,1.f);\n"
+"	texuvV=texuv;\n"
+"}\n";
+
+static const char* fragmentShader= \
+"#version 150\n"
+"\n"
+"in vec4 colourV;\n"
+"out vec4 fragColour;\n"
+"in vec2 texuvV;\n"
+"\n"
+"uniform sampler2D Diffuse;\n"
+"\n"
+"void main(void)\n"
+"{\n"
+"	vec4 texcolorred = texture(Diffuse,texuvV);\n"
+"//	vec4 texcolor = vec4(texcolorred.x,texcolorred.x,texcolorred.x,texcolorred.x);\n"
+"	vec4 texcolor = vec4(1,1,1,texcolorred.x);\n"
+"\n"
+"    fragColour = colourV*texcolor;\n"
+"}\n";
+
+
+void loadShader(){
+	sData.m_shaderProg= gltLoadShaderPair(vertexShader,fragmentShader);
+    
+    sData.m_positionUniform = glGetUniformLocation(sData.m_shaderProg, "p");
+    if (sData.m_positionUniform < 0) {
+		assert(0);
+	}
+	sData.m_colourAttribute = glGetAttribLocation(sData.m_shaderProg, "colour");
+	if (sData.m_colourAttribute < 0) {
+        assert(0);
+    }
+	sData.m_positionAttribute = glGetAttribLocation(sData.m_shaderProg, "position");
+	if (sData.m_positionAttribute < 0) {
+		assert(0);
+  	}
+	sData.m_textureAttribute = glGetAttribLocation(sData.m_shaderProg,"texuv");
+	if (sData.m_textureAttribute < 0) {
+		assert(0);
+	}
+    
+}
+
+void display() {
+    
+    glUseProgram(sData.m_shaderProg);
+    glBindBuffer(GL_ARRAY_BUFFER, sData.m_vertexBuffer);
+    glBindVertexArray(sData.m_vertexArrayObject);
+    
+    GLuint err = glGetError();
+    assert(err==GL_NO_ERROR);
+    
+    vec2 p( 0.f,0.f);//?b?0.5f * sinf(timeValue), 0.5f * cosf(timeValue) );
+    glUniform2fv(sData.m_positionUniform, 1, (const GLfloat *)&p);
+    
+    glEnableVertexAttribArray(sData.m_positionAttribute);
+    glEnableVertexAttribArray(sData.m_colourAttribute);
+	glEnableVertexAttribArray(sData.m_textureAttribute);
+    
+    glVertexAttribPointer(sData.m_positionAttribute, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const GLvoid *)0);
+    glVertexAttribPointer(sData.m_colourAttribute  , 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const GLvoid *)sizeof(vec4));
+    glVertexAttribPointer(sData.m_textureAttribute , 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const GLvoid *)(sizeof(vec4)+sizeof(vec4)));
+	err = glGetError();
+    assert(err==GL_NO_ERROR);
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, sData.m_indexBuffer);
+    
+    err = glGetError();
+    assert(err==GL_NO_ERROR);
+    
+}
+
 int main(int argc, const char * argv[])
 {
+    GLint err;
     GLFWwindow* window;
     
     /* Initialize the library */
@@ -52,18 +251,143 @@ int main(int argc, const char * argv[])
 		return -1;
 	}
     
+    
+    err = glGetError();
+    //assert(err==GL_NO_ERROR);
+    
+    //font rendering init stuff
+    
+    loadShader();
+    loadBufferData();
+    initTestTexture();
+    
+	struct sth_stash* stash = 0;
+	float sx,sy,dx,dy;
+	int droidRegular, droidItalic, droidBold, droidJapanese;
+
+	int fontTextureWidth = 512;
+	int fontTextureHeight = 512;
+	SimpleOpenGL2RenderCallbacks* renderCallbacks = new SimpleOpenGL2RenderCallbacks(&sData);
+
+	stash = sth_create(fontTextureWidth,fontTextureHeight,renderCallbacks);
+    
+    err = glGetError();
+    assert(err==GL_NO_ERROR);
+    
+	if (!stash)
+	{
+		fprintf(stderr, "Could not create stash.\n");
+		return -1;
+	}
+    
+	// Load the first truetype font from memory (just because we can).
+#ifdef _WIN32
+    const char* fontPath = "../../bin/";
+#else
+    const char* fontPath = "./";
+#endif
+    
+    char fullFontFileName[1024];
+    sprintf(fullFontFileName,"%s%s",fontPath,"DroidSerif-Regular.ttf");
+    
+	droidRegular = sth_add_font(stash,fullFontFileName);
+    sprintf(fullFontFileName,"%s%s",fontPath,"DroidSerif-Italic.ttf");
+	droidItalic = sth_add_font(stash,fullFontFileName);
+    sprintf(fullFontFileName,"%s%s",fontPath,"DroidSerif-Bold.ttf");
+	droidBold = sth_add_font(stash,fullFontFileName);
+    sprintf(fullFontFileName,"%s%s",fontPath,"DroidSansJapanese.ttf");
+    droidJapanese = sth_add_font(stash,fullFontFileName);
+    
+    //end of font init stuff
+    
+    
     OGL triangle;
+    
+    
+    // performance measurement
+    glfwSetTime(0);
+    auto prevt = glfwGetTime();
+    
+    int winWidth, winHeight;
+    int fbWidth, fbHeight;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+        auto t = glfwGetTime();
+        auto dt = t - prevt;
+        prevt = t;
+        
+        //get window size
+   		glfwGetWindowSize(window, &winWidth, &winHeight);
+		glfwGetFramebufferSize(window, &fbWidth, &fbHeight);
+		// Calculate pixel ratio for hi-dpi devices.
+		//auto pxRatio = (float)fbWidth / (float)winWidth;
+		glViewport(0, 0, fbWidth, fbHeight);
+
         /* Render here */
 		glClearColor(0.5,0.5,0.5,1);//.4, .4, 0.4, 1.0);
 		glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
         
+        //render text
+        stringstream textOut;
+        textOut << "font frame time: " << std::setprecision(4) << dt << "ms";
+        stringstream textFPS;
+        textFPS << "FPS: " << to_string(1.0f/dt);
+        
+        err = glGetError();
+        assert(err==GL_NO_ERROR);
+        
+		glClearColor(0.5,0.5,0.5,1);//.4, .4, 0.4, 1.0);
+		glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+        
+		// Update and render
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        err = glGetError();
+        assert(err==GL_NO_ERROR);
+        
+		sth_begin_draw(stash);
+		
+		display();
+        
+		sx = 0; sy = fbHeight;
+		dx = sx; dy = sy;
+		int spacing = 512;
+		if (1)
+            //for (int i=20;i<=60;i+=20)
+		{
+			//char txt[512];
+			//sprintf(txt,"%d. The quick brown fox jumper over the lazy dog. 1234567890",i);
+            const char *txt = textOut.str().c_str();
+            sth_draw_text(stash, droidRegular,40.f, 10, dy-spacing, txt, &dx,fbWidth,fbHeight);
+            spacing-=20;
+            const char *txt2 = textFPS.str().c_str();
+            sth_draw_text(stash, droidRegular,25.f, 10, dy-spacing, txt2, &dx,fbWidth,fbHeight);
+            spacing-=20;
+		}
+        
+        //sth_flush_draw(stash);
+        
+        dx = 0;
+        sth_draw_text(stash, droidRegular,16.f, dx, dy-80, "How does this OpenGL True Type font look? ", &dx,fbWidth,fbHeight);
+        
+		sth_end_draw(stash);
+		
+		glEnable(GL_DEPTH_TEST);
+        err = glGetError();
+        assert(err==GL_NO_ERROR);
+        
+        
+        
+        
+        
+        
+        //render triangle
+#if 1
         glUseProgram(triangle.shaderProgram);
         glBindVertexArray(triangle.vao);
         glDrawArrays(GL_TRIANGLES, 0, 3);
-        
+#endif
         
         /* Swap front and back buffers */
         glfwSwapBuffers(window);

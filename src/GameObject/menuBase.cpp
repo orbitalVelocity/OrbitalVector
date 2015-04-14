9f46721513ce54078ed37ce71eae6eecbb291e3e
omikun <omikun@gmail.com>
2015-04-14 12:21:20 -0400

    started work on a menu base class from which all UI will derive. Also bug fixes on missileSystem to handle cases where target is destroyed before the missile get hits

---
+++ src/GameObject/menuBase.cpp
@@ -0,0 +1,9 @@
+//
+//  MenuBase.cpp
+//  OrbitalVector
+//
+//  Created by Si Li on 4/14/15.
+//  Copyright (c) 2015 Si Li. All rights reserved.
+//
+
+#include "MenuBase.h"
---
+++ src/GameObject/menuBase.h
@@ -0,0 +1,14 @@
+//
+//  MenuBase.h
+//  OrbitalVector
+//
+//  Created by Si Li on 4/14/15.
+//  Copyright (c) 2015 Si Li. All rights reserved.
+//
+
+#ifndef __OrbitalVector__MenuBase__
+#define __OrbitalVector__MenuBase__
+
+#include <stdio.h>
+
+#endif /* defined(__OrbitalVector__MenuBase__) */

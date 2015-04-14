32851c020ffa15bf7cf34ab289eac342e277e320
omikun <omikun@gmail.com>
2015-04-14 10:02:30 -0400

    house cleaning : scene.h removed some outdated comments and declarations

--- src/scene.h
+++ src/scene.h
@@ -20,32 +20,12 @@
 extern std::vector<tinyobj::shape_t> shapes;
 extern std::vector<tinyobj::material_t> materials;
 
-/**
- Contains game state for a mission, menus, etc
- 
- Used for campaign missions, skermishes, and full screen menus
-    such as save/load/new game menus
-    
- @code
- OR make menus a different thing entirely
- */
 class Scene {
 public:
     void init();
     void update();
     
 public:
-    
-
-    //lights are entities with self emitters
-        //brightness,           type: cone/bulbs/area/cubic
-        //range (rendering optimization) in meters
-        //falloff (inverse)     linear, square
-    
-    
-    //no need for these classes! get rid of them!
-    friend class UserInput;
-    friend class Renderer;
     
 };
 
